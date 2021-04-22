12ef04a171221a63dfdabc3cdbb528cd17a10ce1
toru.konno <torukonno63@gmail.com>
2021-04-22 23:44:05 +0900

    Add EnditServlet

---
+++ src/controllers/EditServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task2;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのメッセージを1件のみをデータベースから取得
+        Task2 m = em.find(Task2.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("task2",m);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("task2_id", m.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks2/edit.jsp");
+        rd.forward(request, response);
+
+
+    }
+
+}
