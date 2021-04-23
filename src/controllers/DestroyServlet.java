746c293244248698321481fc2432307509b8ee25
toru.konno <torukonno63@gmail.com>
2021-04-23 17:45:57 +0900

    Add UpdateServlet.java

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +13,16 @@ import models.Task2;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public DestroyServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,33 +32,25 @@ public class UpdateServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())){
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             // セッションスコープからメッセージのIDを取得して
             // 該当のIDのメッセージ1件のみをデータベースから取得
-            Task2 m = em.find(Task2.class, (Integer)(request.getSession().getAttribute("task2_id")));
+            Task2 m = em.find(Task2.class , (Integer)(request.getSession().getAttribute("task2_id")));
 
-            // フォームの内容を各フィールドｓに上書き
-            String content = request.getParameter("content");
-            m.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
-
-            // データベースを更新
             em.getTransaction().begin();
+            em.remove(m);           // データ削除
             em.getTransaction().commit();
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("task2_id");
 
-            // indexページへのリダイレクト
-            response.sendRedirect(request.getContextPath() +  "/index");
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
 
         }
-
 
     }
 
